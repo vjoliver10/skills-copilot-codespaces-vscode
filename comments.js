10cc443c9bb3fd87e2b320e2a464c4cd1b539bdf
vjoliver10 <vjoliver10@gmail.com>
2025-02-26 19:57:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,3 @@
-// create web server
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-// create a route for comments/:id/update
+//create web server
+//create a route to handle comments
+//create a route to handle
